7dea59f155f0099b70d65c390b54780a54448faf
EDA <eda@mac-mini-1.local>
2016-08-19 15:21:55 +1200

    name adding complete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Team from './components/Team'
+import NameInput from './components/NameInput'
 
+function test () {
+  console.log(test)
+}
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Team members= {["Mandy", "Siobhan", "Sam", "Squirrel", "hawks"]} teamname= {"Zelda"} teamicon= {"Square"} membericon= {"Star"}/>,
+    <div>
+      <Team members= {["Mandy", "Siobhan", "Sam", "Squirrel", "hawks"]} teamname= {"Zelda"} teamicon= {"Square"} membericon= {"Star"}/>
+      <NameInput onInputNameChange={test} />
+    </div>,
     document.getElementById('app')
   )
 })
